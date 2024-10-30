5b2f79a4e421a982e9ecd69824fdf3e4c79b0e4d
Am-Kitsune <105692663+AM-kitsune@users.noreply.github.com>
2024-10-30 09:31:48 +0700

    Add files via upload

---
+++ ArrayNilai13.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class ArrayNilai13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] nilaiAkhir = new int[10];
+        for (int i = 0; i < 10; i++){
+            System.out.print("Masukan Nilai Akhir ke- " + i + " : ");
+            nilaiAkhir[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++){
+            System.out.println("Masukan Nilai Akhir ke- " + i + " adalah " + nilaiAkhir[i] );
+            
+        }
+    }
+}
